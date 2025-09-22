76295ee9eaa80b6b55c20b645163c372abcbfed5
NecroPal <tahernawab5253@gmail.com>
2025-09-22 11:30:58 +0530

    initial commit

---
+++ src/Landing Page.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './Landing Page.css'
+
+function App() {  
+
+  return (
+    <>
+  <div className="body">
+    <div className="overlay">
+    <div className="container">
+      <h1>Welcome to JUMAN-JEE</h1>
+      <p>Experience seamless conversations like never before.</p>
+      <a href="profile.html" className="btn">Get Started</a>
+    </div>
+  </div>  
+  </div>
+    </>
+  )
+}
+
+export default App
