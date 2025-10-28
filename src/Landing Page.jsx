fc7a62c9819ce0d2eae4978430f939b39f761a3b
NecroPal <tahernawab5253@gmail.com>
2025-10-29 02:04:33 +0530

    Update Landing Page.jsx

--- src/Landing Page.jsx
+++ src/Landing Page.jsx
@@ -8,7 +8,7 @@ function App() {
   <div className="body">
     <div className="overlay">
     <div className="container">
-      <h1>Welcome to JUMAN-JEE</h1>
+      <h1>Welcome to Jibber</h1>
       <p>Experience seamless conversations like never before.</p>
       <a href="profile.html" className="btn">Get Started</a>
     </div>
